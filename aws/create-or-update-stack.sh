b708047b03495bb0fdfb3f19713e741beff8c671
Mark Wainwright <markwainwright1@gmail.com>
2018-03-16 16:22:06 -0700

    Add error alarm

--- aws/create-or-update-stack.sh
+++ aws/create-or-update-stack.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 set -e
 
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 STACK_NAME="la-crosse-to-wunderground-pws"
 CODE_S3_BUCKET="[your s3 bucket]"
+ALARM_EMAIL="[your email address]"
 
 CODE_FILENAME="$STACK_NAME-$(date +%FT%T).zip"
 
 npm test
 
-pushd lambda
+pushd "$THIS_DIR"
 rm -rf dist
 mkdir -p dist
-cp lambda.js dist
-cp ../package.json dist
-cp -r ../lib dist
-
 pushd dist
+cp ../lambda.js .
+cp ../../package.json .
+cp -r ../../lib .
 NODE_ENV=production npm install
 rm -r package.json node_modules/*/{README.md,test*,bower.json,LICENSE}
 zip -qr code.zip .
 popd
 
-aws s3 cp dist/code.zip "s3://$CODE_S3_BUCKET/$CODE_FILENAME"
-
-popd
+aws s3 cp "$THIS_DIR/dist/code.zip" "s3://$CODE_S3_BUCKET/$CODE_FILENAME"
 
 ACTION="update"
 aws cloudformation describe-stacks --stack-name "$STACK_NAME" || ACTION="create"
 
 aws cloudformation "$ACTION-stack" \
   --stack-name "$STACK_NAME" \
-  --template-body "file://lambda/cloudformation-template.json" \
+  --template-body "file://$THIS_DIR/cloudformation-template.yaml" \
   --capabilities "CAPABILITY_NAMED_IAM" \
   --parameters \
-    "ParameterKey=StackName,ParameterValue=$STACK_NAME" \
+    "ParameterKey=AlarmEmail,ParameterValue=$ALARM_EMAIL" \
     "ParameterKey=CodeS3Bucket,ParameterValue=$CODE_S3_BUCKET" \
-    "ParameterKey=CodeObjectKey,ParameterValue=$CODE_FILENAME" \
+    "ParameterKey=CodeS3Key,ParameterValue=$CODE_FILENAME" \
     "ParameterKey=LaCrosseDeviceId,ParameterValue=$LA_CROSSE_DEVICE_ID" \
     "ParameterKey=WundergroundId,ParameterValue=$WUNDERGROUND_ID" \
-    "ParameterKey=WundergroundPassword,ParameterValue=$WUNDERGROUND_PASSWORD"
+    "ParameterKey=WundergroundPassword,ParameterValue=$WUNDERGROUND_PWD"
 
 aws cloudformation wait "stack-$ACTION-complete" --stack-name "$STACK_NAME"
 
