cf22c4f29ef49bd17d20b80e864f421fa5ccc134
Mark Wainwright <markwainwright1@gmail.com>
2018-03-16 17:13:29 -0700

    Avoid unnecessary updates when lambda code hasn't changed

--- aws/create-or-update-stack.sh
+++ aws/create-or-update-stack.sh
@@ -7,8 +7,6 @@ STACK_NAME="la-crosse-to-wunderground-pws"
 CODE_S3_BUCKET="[your s3 bucket]"
 ALARM_EMAIL="[your email address]"
 
-CODE_FILENAME="$STACK_NAME-$(date +%FT%T).zip"
-
 npm test
 
 pushd "$THIS_DIR"
@@ -20,13 +18,18 @@ cp ../../package.json .
 cp -r ../../lib .
 NODE_ENV=production npm install
 rm -r package.json node_modules/*/{README.md,test*,bower.json,LICENSE}
-zip -qr code.zip .
+hash="$(find . -type f -print0 | xargs -0 shasum | shasum | cut -d " " -f1)"
+zip -X --quiet --recurse-paths code.zip .
 popd
 
-aws s3 cp "$THIS_DIR/dist/code.zip" "s3://$CODE_S3_BUCKET/$CODE_FILENAME"
+code_filename_local="$THIS_DIR/dist/code.zip"
+code_filename_s3="$STACK_NAME-$hash.zip"
+
+aws s3 ls "s3://$CODE_S3_BUCKET/$code_filename_s3" || \
+  aws s3 cp "$code_filename_local" "s3://$CODE_S3_BUCKET/$code_filename_s3"
 
 ACTION="update"
-aws cloudformation describe-stacks --stack-name "$STACK_NAME" || ACTION="create"
+aws cloudformation describe-stacks --stack-name "$STACK_NAME" > /dev/null || ACTION="create"
 
 aws cloudformation "$ACTION-stack" \
   --stack-name "$STACK_NAME" \
@@ -35,7 +38,7 @@ aws cloudformation "$ACTION-stack" \
   --parameters \
     "ParameterKey=AlarmEmail,ParameterValue=$ALARM_EMAIL" \
     "ParameterKey=CodeS3Bucket,ParameterValue=$CODE_S3_BUCKET" \
-    "ParameterKey=CodeS3Key,ParameterValue=$CODE_FILENAME" \
+    "ParameterKey=CodeS3Key,ParameterValue=$code_filename_s3" \
     "ParameterKey=LaCrosseDeviceId,ParameterValue=$LA_CROSSE_DEVICE_ID" \
     "ParameterKey=WundergroundId,ParameterValue=$WUNDERGROUND_ID" \
     "ParameterKey=WundergroundPassword,ParameterValue=$WUNDERGROUND_PWD"
