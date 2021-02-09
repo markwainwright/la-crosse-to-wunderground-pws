f5691e06e2bdd8764719feb86162f8fdcb1520ce
Mark Wainwright <markwainwright1@gmail.com>
2021-02-09 00:55:26 -0800

    Log request and message IDs

--- src/s3-writer.ts
+++ src/s3-writer.ts
@@ -1,6 +1,7 @@
 import { SNSEvent } from 'aws-lambda';
 
 import writeToS3 from './lib/writeToS3';
+import { ReaderLambdaMessage } from './lib/types';
 
 const { S3_BUCKET_NAME } = process.env;
 
@@ -9,15 +10,26 @@ export async function handler(event: SNSEvent) {
     throw new Error('No S3_BUCKET_NAME defined');
   }
 
-  const messages = event.Records.map(record => JSON.parse(record.Sns.Message));
+  const snsRecords = event.Records;
 
   await Promise.all(
-    messages.map(async message => {
-      const observations = message.responsePayload;
+    snsRecords.map(async (snsRecord) => {
+      const snsMessageId = snsRecord.Sns.MessageId;
+
+      const lambdaMessage = JSON.parse(snsRecord.Sns.Message) as ReaderLambdaMessage;
+      const requestId = lambdaMessage.requestContext.requestId;
+      const observations = lambdaMessage.responsePayload;
 
       const s3Url = await writeToS3(S3_BUCKET_NAME, observations);
 
-      console.log(JSON.stringify({ observations, s3Url }));
+      console.log(
+        JSON.stringify({
+          observations,
+          s3Url,
+          snsMessageId,
+          requestId,
+        })
+      );
     })
   );
 }
