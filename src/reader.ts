69d6b722540d8dfcface882767fffcff5ba33d60
Mark Wainwright <markwainwright1@gmail.com>
2021-02-09 00:55:26 -0800

    Log request and message IDs

--- src/reader.ts
+++ src/reader.ts
@@ -1,9 +1,11 @@
+import { Context, EventBridgeEvent } from 'aws-lambda';
+
 import getLaCrosseObservations from './lib/getLaCrosseObservations';
 import convertLaCrosseObservations from './lib/convertLaCrosseObservations';
 
-export async function handler() {
-  const { LA_CROSSE_DEVICE_ID } = process.env;
+const { LA_CROSSE_DEVICE_ID } = process.env;
 
+export async function handler(event: EventBridgeEvent<'Scheduled Event', {}>, context: Context) {
   if (!LA_CROSSE_DEVICE_ID) {
     throw new Error('No LA_CROSSE_DEVICE_ID defined');
   }
@@ -17,6 +19,8 @@ export async function handler() {
       laCrosseObservations,
       observations,
       deviceId: LA_CROSSE_DEVICE_ID,
+      requestId: context.awsRequestId,
+      eventId: event.id,
     })
   );
 
